0f84a36a929261875732c0bb07ecdd1a5434af3e
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-02-01 08:05:03 -0800

    note entity

---
+++ app/src/main/java/com/jmzd/ghazal/noteappmvp/data/model/NoteEntity.kt
@@ -0,0 +1,16 @@
+package com.jmzd.ghazal.noteappmvp.data.model
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.jmzd.ghazal.noteappmvp.utils.NOTE_TABLE
+
+@Entity(tableName = NOTE_TABLE)
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    var title: String = "",
+    var desc: String = "",
+    var category: String = "",
+    var priority: String = ""
+)
