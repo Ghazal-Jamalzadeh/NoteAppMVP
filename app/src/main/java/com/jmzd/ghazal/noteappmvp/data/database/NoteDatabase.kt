d6c52fd1fa8f14de9f5df761a6bfc96d2b6f2a12
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-02-03 04:05:13 -0800

    ui packages

--- app/src/main/java/com/jmzd/ghazal/noteappmvp/data/database/NoteDatabase.kt
+++ app/src/main/java/com/jmzd/ghazal/noteappmvp/data/database/NoteDatabase.kt
@@ -1,7 +1,5 @@
 package com.jmzd.ghazal.noteappmvp.data.database
 
-
-import academy.nouri.s3_mvp.room.data.database.NoteDao
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.jmzd.ghazal.noteappmvp.data.model.NoteEntity
