20b9ff666f479593e2ada91a46661dff9b0ba927
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-02-03 05:34:59 -0800

    open bottomSheetDilaog onClick

--- app/src/main/java/com/jmzd/ghazal/noteappmvp/ui/main/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/noteappmvp/ui/main/MainActivity.kt
@@ -3,10 +3,26 @@ package com.jmzd.ghazal.noteappmvp.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jmzd.ghazal.noteappmvp.R
+import com.jmzd.ghazal.noteappmvp.databinding.ActivityMainBinding
+import com.jmzd.ghazal.noteappmvp.ui.add.NoteFragment
 
 class MainActivity : AppCompatActivity() {
+
+    //binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initViews
+        binding.apply {
+            //Note detail
+            addNoteBtn.setOnClickListener {
+                NoteFragment().show(supportFragmentManager, NoteFragment().tag) }
+        }
+
     }
 }
