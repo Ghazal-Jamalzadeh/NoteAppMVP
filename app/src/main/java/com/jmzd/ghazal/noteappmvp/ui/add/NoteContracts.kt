47efcc0ef98f76290162f28f71742bd274c39566
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-02-04 05:04:50 -0800

    note contracts

---
+++ app/src/main/java/com/jmzd/ghazal/noteappmvp/ui/add/NoteContracts.kt
@@ -0,0 +1,17 @@
+package com.jmzd.ghazal.noteappmvp.ui.add
+
+import com.jmzd.ghazal.noteappmvp.base.BasePresenter
+import com.jmzd.ghazal.noteappmvp.data.model.NoteEntity
+
+interface NoteContracts {
+
+    interface View {
+        /* این close مربوط به بعد از زدن دکمه سیو است. دکمه بستن معمولی نیاز به مدیریت شدن از طرف پرزنتر ندارد
+        * زمانی نیاز داریم که به واسه اطلاعاتی که از پرزنتز میاد قراره تغییری در ویوی ما اتفاق بیفته */
+        fun close()
+    }
+
+    interface Presenter : BasePresenter {
+        fun saveNote(entity: NoteEntity)
+    }
+}
