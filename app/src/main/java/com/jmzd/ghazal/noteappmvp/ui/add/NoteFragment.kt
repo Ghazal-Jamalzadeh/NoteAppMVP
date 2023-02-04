5daeb1e60e1566c84447ecc7f0951a908aa79509
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-02-04 04:31:51 -0800

    fill spinners

--- app/src/main/java/com/jmzd/ghazal/noteappmvp/ui/add/NoteFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/noteappmvp/ui/add/NoteFragment.kt
@@ -1,17 +1,28 @@
 package com.jmzd.ghazal.noteappmvp.ui.add
 
+import android.R
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.jmzd.ghazal.noteappmvp.databinding.FragmentNoteBinding
+import com.jmzd.ghazal.noteappmvp.utils.*
 
 
 class NoteFragment : BottomSheetDialogFragment() {
 
+    //Binding
     private lateinit var binding : FragmentNoteBinding
+
+    //Other
+    private lateinit var categoriesList: Array<String>
+    private var category = ""
+    private lateinit var prioritiesList: Array<String>
+    private var priority = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +37,44 @@ class NoteFragment : BottomSheetDialogFragment() {
         binding.apply {
             //Close
             closeImg.setOnClickListener { this@NoteFragment.dismiss() }
+            //Spinners
+            categoriesSpinnerItems()
+            prioritiesSpinnerItems()
+        }
+    }
+
+
+    private fun categoriesSpinnerItems() {
+        categoriesList = arrayOf(WORK, HOME, EDUCATION, HEALTH)
+        val adapter = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, categoriesList)
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        binding.categoriesSpinner.adapter = adapter
+        binding.categoriesSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                category = categoriesList[p2]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+    }
+
+    private fun prioritiesSpinnerItems() {
+        prioritiesList = arrayOf(HIGH, NORMAL, LOW)
+        val adapter = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, prioritiesList)
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        binding.prioritySpinner.adapter = adapter
+        binding.prioritySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                priority = prioritiesList[p2]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
         }
     }
 
